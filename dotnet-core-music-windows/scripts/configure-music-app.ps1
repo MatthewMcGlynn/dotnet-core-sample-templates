a62e7874763725e451b77f760c98608860531449
MatthewMcGlynn <mattmcg@microsoft.com>
2017-04-27 19:03:25 -0700

    Update dotnet core package link

--- dotnet-core-music-windows/scripts/configure-music-app.ps1
+++ dotnet-core-music-windows/scripts/configure-music-app.ps1
@@ -22,8 +22,8 @@ Install-WindowsFeature web-server -IncludeManagementTools
 # Install dot.net core sdk
 Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
 Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
-Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
-Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-dev-win-x64.latest.exe -outfile c:\temp\DotNetCore.exe
+Start-Process c:\temp\DotNetCore.exe -ArgumentList '/quiet' -Wait
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
 Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
@@ -31,6 +31,7 @@ Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 Invoke-WebRequest  https://github.com/MatthewMcGlynn/dotnet-core-sample-templates/raw/master/dotnet-core-music-windows/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
 Expand-Archive C:\temp\musicstore.zip c:\music
 
+cd c:\music\
 # Azure SQL connection sting in environment variable
 [Environment]::SetEnvironmentVariable("Data:DefaultConnection:ConnectionString", "Server=$sqlserver;Database=MusicStore;Integrated Security=False;User Id=$user;Password=$password;MultipleActiveResultSets=True;Connect Timeout=30",[EnvironmentVariableTarget]::Machine)
 
