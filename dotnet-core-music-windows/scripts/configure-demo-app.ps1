950f143f6bbbff3d5e6806a3239430dab56c3ef8
MatthewMcGlynn <mattmcg@microsoft.com>
2017-06-18 15:07:23 -0700

    Use demo script

--- dotnet-core-music-windows/scripts/configure-demo-app.ps1
+++ dotnet-core-music-windows/scripts/configure-demo-app.ps1
@@ -22,7 +22,7 @@ Install-WindowsFeature web-server -IncludeManagementTools
 # Install dot.net core sdk
 Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
 Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
-Invoke-WebRequest https://dotnetcli.blob.core.windows.net/dotnet/Sdk/master/dotnet-dev-win-x64.latest.exe -outfile c:\temp\DotNetCore.exe
+Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848740 -outfile c:\temp\DotNetCore.exe
 Start-Process c:\temp\DotNetCore.exe -ArgumentList '/quiet' -Wait
 Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
 Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
